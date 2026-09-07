336a0b93d4f356d632eccffe3fdffde9333ab408
Danil Nagaev <danilnagaev78@gmail.com>
2026-09-07 22:44:46 +0500

    Добавил списки тк в прошлый коммит не сохранилось

--- Projects/Student Course Manager/Program.cs
+++ Projects/Student Course Manager/Program.cs
@@ -8,13 +8,12 @@ namespace Student_Course_Manager
 {
     internal class Program
     {
+        List<Student> students = new List<Student>();
+        List<Course> courses = new List<Course>();
+        List<Enrollment> enrollments = new List<Enrollment>();
         static void Main(string[] args)
         {
-            var student = new Student(1, "Иван", "Петров", 20, "ivan@mail.ru");
-            student.Print();
-            var date = DateTime.Now;
-            var enrolment = new Enrollment(1, 1, 1, date, null);
-            enrolment.Print();
+            
         }
     }
 }
