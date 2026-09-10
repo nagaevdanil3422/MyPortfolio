ef5e94f428e1456a3d4c48c7add3a00d112d2ea7
Danil Nagaev <danilnagaev78@gmail.com>
2026-09-10 21:36:52 +0500

    Initial commit

---
+++ Projects/ToDoList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приветик!");
+        }
+    }
+}
